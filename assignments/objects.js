07c5ca1cc68295bb118b03a78b0d14f529216c8e
Roger Lee <rmlee0003@gmail.com>
2019-04-17 20:10:25 -0400

    added logs of objects values and speaking names

--- assignments/objects.js
+++ assignments/objects.js
@@ -108,5 +108,6 @@ console.log(parent.child.grandchild.name + " " + parent.child.grandchild.age);
 // Have the parent speak
 console.log(parent.speak());
 // Have the child speak
-
+console.log(parent.child.speak());
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak());
